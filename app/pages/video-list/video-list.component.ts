ed66c6ea684773cefcc5077b035ec8ab7228a954
Caue Queiroz <cauenqueiroz@gmail.com>
2017-10-06 20:24:18 -0300

    Create loading

--- app/pages/video-list/video-list.component.ts
+++ app/pages/video-list/video-list.component.ts
@@ -4,9 +4,15 @@ import * as appConfig from 'application-settings';
 @Component({
     moduleId: module.id,
     selector: 'video-list',
-    templateUrl: './video-list.component.html'
+    templateUrl: './video-list.component.html',
+    styleUrls: ['./video-list-common.css']
 })
 export class VideoListComponent {
 
     private token = appConfig.getString('access_token');
+    private isLoading = true;
+
+    constructor() {
+        console.log('Loading facebook data..');
+    }
 }
